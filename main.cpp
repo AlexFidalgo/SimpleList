d8f14289d39ea72b5379983a5993fc561a1a8a38
alex <alexfidalgo10@gmail.com>
2022-04-19 21:01:52 -0300

    V

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "Hello SimpleList!" << std::endl;
+void print_menu(string name);
+void print_list();
+void add_item();
+void delete_item();
+
+vector<string> list;
+
+int main(int arg_count, char *args[]) {
+    if (arg_count > 1) {
+        string name(args[1]);
+        print_menu(name);
+    }
+    else {
+        cout << "Username not supplied" << endl;
+    }
+
     return 0;
+}
+
+void print_menu (string name) {
+    int choice;
+    cout << "***************\n";
+    cout << "1 - Print list.\n2 - Add to list.\n3 - Delete from list.\n4 - Quit.\n";
+    cout << "Enter your choice and press return: ";
+    cin >> choice;
+    if (choice == 4) {
+        exit(0);
+    }
+    else {
+        cout << "Ongoing\n";
+    }
 }
